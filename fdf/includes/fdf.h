bbf4ee91d411c2696f886fafc87c5324a42ad24c
tiger008 <mythom008@gmail.com>
2015-01-19 09:17:58 +0100

    fdf.h

--- fdf/includes/fdf.h
+++ fdf/includes/fdf.h
@@ -12,10 +12,19 @@
 
 #ifndef FDF_H
 # define FDF_H
-int		ft_drawit(int fd);
+# define ERROR -1
+
 typedef	struct	s_env
 {
 	void	*mlx;
 	void	*win;
 }				t_env;
+typedef	struct	s_grid
+{
+	int	*tab;
+	int	li;
+	int	co;
+}				t_grid;
+int		ft_parser(int fd, t_grid *grid);
+void	ft_initgrid(t_grid *grid);
 #endif
