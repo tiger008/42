0645e5d8e92c7ab47471eca5a762f641d2e8e5b8
Thomas PERRAUT <tperraut@e2r2p6.42.fr>
2016-05-17 11:52:42 +0200

    itoa et printf.h

--- fillit/libft/src/ft_itoa.c
+++ fillit/libft/src/ft_itoa.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tperraut <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: tperraut <tperraut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/11 16:10:05 by tperraut          #+#    #+#             */
-/*   Updated: 2014/11/26 12:53:51 by tperraut         ###   ########.fr       */
+/*   Updated: 2016/05/17 09:39:12 by tperraut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 #include "libft.h"
 
@@ -16,53 +18,38 @@ static int	ft_nbdigit(int n)
 {
 	int	digit;
 
-	digit = 0;
-	if (n == 0)
-		return (1);
-	while (n)
+	digit = 1;
+	while (n > 9)
 	{
 		n = n / 10;
-		digit++;
+		++digit;
 	}
 	return (digit);
 }
 
-static int	ft_tenpower(int power)
-{
-	int	result;
-	int	i;
-
-	i = 0;
-	result = 1;
-	while (i++ < power)
-		result = result * 10;
-	return (result);
-}
-
 char		*ft_itoa(int n)
 {
-	char			*result;
-	unsigned int	nn;
-	size_t			i;
-	int				digit;
+	char	*str;
+	int		neg;
+	int		digit;
 
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	neg = (n < 0) ? 1 : 0;
+	n = (n < 0) ? -n : n;
 	digit = ft_nbdigit(n);
-	i = 0;
-	result = (char*)malloc(sizeof(char) * (digit + 2));
-	if (!result)
+	if (!(str = (char*)malloc(sizeof(char) * (digit + ((n < 0) ? 2 : 1)))))
 		return (NULL);
-	if (n < 0)
-	{
-		result[i++] = '-';
-		nn = (unsigned int)-n;
-	}
 	else
-		nn = (unsigned int)n;
-	while (digit)
 	{
-		result[i++] = '0' + (char)((nn / ft_tenpower(digit - 1)) % 10);
-		digit--;
+		if (neg)
+			str[0] = '-';
+		str[digit] = '\0';
+		while (digit--)
+		{
+			str[digit + neg] = (int)('0' + n % 10);
+			n /= 10;
+		}
 	}
-	result[i] = '\0';
-	return (result);
+	return (str);
 }
--- ft_printf/include/libftprintf.h
+++ ft_printf/include/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: tperraut <tperraut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/26 16:22:02 by tperraut          #+#    #+#             */
-/*   Updated: 2016/04/30 18:54:43 by tperraut         ###   ########.fr       */
+/*   Updated: 2016/05/17 11:10:34 by tperraut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,19 @@
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 int		ft_strcmp(const char *s1, const char *s2);
-char	*ft_itoa(int n);
+char	*ft_itoabase(int n, int base);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 int		ft_printf(const char *format, ...);
 
 #endif
+
+
+
+
+
+
+
+
+
+
