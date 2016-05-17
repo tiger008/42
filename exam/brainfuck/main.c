cf6faa6944f8f125406f1210ab005a83bce05b01
Thomas PERRAUT <tperraut@e2r2p6.42.fr>
2016-05-17 11:27:08 +0200

    brainfuck good

--- exam/brainfuck/main.c
+++ exam/brainfuck/main.c
@@ -1,100 +1,65 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/06/03 09:27:11 by exam              #+#    #+#             */
-/*   Updated: 2015/06/03 16:09:54 by tperraut         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "brainfuck.h"
 #include <unistd.h>
-#include <stdlib.h>
 
-char	*ft_bzero(int size)
+int	main(int ac, char **av)
 {
-	char	*result;
-
-	if (!(result = (char *)malloc(sizeof(char) * (size + 1))))
-		return (NULL);
-	result[size] = '\0';
-	while (size--)
-		result[size] = '\0';
-	return (result);
-}
-
-void	ft_begin(int t, int **p, char **av, int **i)
-{
-	(**p)++;
-	while (av[1][**i] && t != **p)
-	{
-		(**i)++;
-		if (av[1][**i] == ']')
-			(**p)--;
-		if (av[1][**i] == '[')
-			(**p)++;
-	}
-	return ;
-}
-
-void	ft_end(int t, int **p, char **av, int **i)
-{
-	(**p)--;
-	while (av[1][**i] && t != **p)
-	{
-		(**i)--;
-		if (av[1][**i] == '[')
-			(**p)++;
-		if (av[1][**i] == ']')
-			(**p)--;
-	}
-	(**i)--;
-	return ;
-}
-
-int		ft_rbf(char **t, char **av, int *i, int *p)
-{
-	int	temp;
-
-	if (av[1][*i] == '\0')
-		return (0);
-	temp = *p;
-	if (av[1][*i] == '+')
-		(**t)++;
-	if (av[1][*i] == '-')
-		(**t)--;
-	if (av[1][*i] == '<')
-		(*t)--;
-	if (av[1][*i] == '>')
-		(*t)++;
-	if (av[1][*i] == '.')
-		write (1, *t, 1);
-	if (av[1][*i] == '[' && **t == '\0')
-		ft_begin(temp, &p, av, &i);
-	if (av[1][*i] == ']' && **t != '\0')
-		ft_end(temp, &p, av, &i);
-	if (av[1][*i] == '[')
-		(*p)++;
-	if (av[1][*i] == ']')
-		(*p)--;
-	(*i)++;
-	return (ft_rbf(t, av, i, p));
-}
-
-int		main(int ac, char **av)
-{
-	char	*t;
+	char	out[2048] = {0};
+	char	*in;
 	int		i;
-	int		p;
+	int		par;
+	int		tmp;
 
-	i = 0;
-	p = 0;
-	t = ft_bzero(MAX);
-	if (ac == 2 && t)
-		return (ft_rbf(&t, av, &i, &p));
+	if (ac == 2)
+	{
+		in = av[1];
+		par = 0;
+		i = 0;
+		while (*in)
+		{
+			if (*in == '>')
+				i++;
+			else if (*in == '<')
+				i--;
+			else if (*in == '+')
+				out[i]++;
+			else if (*in == '-')
+				out[i]--;
+			else if (*in == '.')
+				write(1, &out[i], 1);
+			else if (*in == '[')
+			{
+				par++;
+				if (out[i] == '\0')
+				{
+					tmp = par - 1;
+					while (par != tmp)
+					{
+						in++;
+						if (*in == '[')
+							par++;
+						if (*in == ']')
+							par--;
+					}
+				}
+			}
+			else if (*in == ']')
+			{
+				par--;
+				if (out[i] != '\0')
+				{
+					tmp = par + 1;
+					while (par != tmp)
+					{
+						in--;
+						if (*in == '[')
+							par++;
+						if (*in == ']')
+							par--;
+					}
+				}
+			}
+			in++;
+		}
+	}
 	else
 		write(1, "\n", 1);
 	return (0);
