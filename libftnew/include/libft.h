ae70d4f0ef2413671b317777929a96767b4b5861
tiger008 <mythom008@gmail.com>
2016-05-20 00:24:50 +0200

    libftnew

--- libftnew/include/libft.h
+++ libftnew/include/libft.h
@@ -6,27 +6,41 @@
 /*   By: tperraut <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 15:34:39 by tperraut          #+#    #+#             */
-/*   Updated: 2015/01/10 19:04:11 by tperraut         ###   ########.fr       */
+/*   Updated: 2016/05/19 22:29:05 by tperraut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
-
 # define LIBFT_H
-# define TRUE 1
-# define FALSE 0
-# define ABS(X) (X < 0) ? -X : X
+
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-typedef	struct	s_list
-{
-	void			*content;
-	size_t			content_size;
-	struct s_list	*next;
-}				t_list;
-int				ft_atoi(const char *str);
+# define TRUE 1
+# define FALSE 0
+# define ERR -1
+
+# define ABS(X)		(X < 0 ? -X : X)
+# define MAX(X, Y)	(X < Y ? Y : X)
+# define MIN(X, Y)	(X < Y ? X : Y)
+# define SIGN(X)	(X < 0 ? -1 : 1)
+
+# define T(expr, yes, no)	(expr ? yes : no)
+# define GOTO(lbl)			goto lbl
+# define FOR(a, b, c)		0){;} for ((a); (b); (c)
+# define SWITCH(x)			0){;} switch (x
+# define CASE(x);			case x:
+# define INIT(t, n, v)		t n = v;
+
+# define RETURN_IF(x, y)	if (x) return (y);
+
+/*
+ ** ----------
+ ** | MEMORY |
+ ** ----------
+ */
+
 void			*ft_memset(void *b, int c, size_t len);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
@@ -34,6 +48,29 @@ void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memalloc(size_t size);
+void			ft_memdel(void **ap);
+
+/*
+ ** --------
+ ** | CHAR |
+ ** --------
+ */
+
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+
+/*
+ ** ----------
+ ** | STRING |
+ ** ----------
+ */
+
 size_t			ft_strlen(const char *s);
 char			*ft_strdup(const char *s1);
 char			*ft_strcpy(char *dst, const char *src);
@@ -47,15 +84,6 @@ char			*ft_strstr(const char *s1, const char *s2);
 char			*ft_strnstr(const char *s1, const char *s2, size_t n);
 int				ft_strcmp(const char *s1, const char *s2);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
-int				ft_isalpha(int c);
-int				ft_isdigit(int c);
-int				ft_isalnum(int c);
-int				ft_isascii(int c);
-int				ft_isprint(int c);
-int				ft_toupper(int c);
-int				ft_tolower(int c);
-void			*ft_memalloc(size_t size);
-void			ft_memdel(void **ap);
 char			*ft_strnew(size_t size);
 void			ft_strdel(char **as);
 void			ft_strclr(char *s);
@@ -69,7 +97,38 @@ char			*ft_strsub(char const *s, unsigned int start, size_t len);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s);
 char			**ft_strsplit(char const *s, char c);
+
+int				ft_atoi(const char *str);
+
 char			*ft_itoa(int n);
+
+
+/*
+ ** ---------------
+ ** | LINKED LIST |
+ ** ---------------
+ */
+
+typedef struct	s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}				t_list;
+
+t_list			*ft_lstnew(void const *content, size_t content_size);
+void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void			ft_lstadd(t_list **alst, t_list *new);
+void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
+/*
+ ** -----------
+ ** | DISPLAY |
+ ** -----------
+ */
+
 void			ft_putchar(char c);
 void			ft_putstr(char const *s);
 void			ft_putendl(char const *s);
@@ -78,12 +137,22 @@ void			ft_putnbr(int n);
 void			ft_putstr_fd(char const *s, int fd);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putnbr_fd(int n, int fd);
-t_list			*ft_lstnew(void const *content, size_t content_size);
-void			ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
-void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
-void			ft_lstadd(t_list **alst, t_list *new);
-void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
-t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+
+/*
+ ** --------
+ ** | MATH |
+ ** --------
+ */
+
 int				ft_abs(int n);
+int				ft_sqrt(int n);
+
+	/*
+	 ** ---------
+	 ** | DEBUG |
+	 ** ---------
+	 */
+
+void			ft_error(char const *s);
 
 #endif
